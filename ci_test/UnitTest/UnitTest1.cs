da98e1273d61b8a83d94de7d11bf2c18a31a1059
jun <jun_aok@aol.com>
2019-06-12 18:10:17 +0900

    プロジェクト作成

---
+++ ci_test/UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+            Assert.AreEqual((2 * 10), 20);
+        }
+    }
+}
